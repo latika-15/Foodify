0016a75c421cdad1117ac5228a6dd8a340773726
latika-15 <lavitewari15@gamil.com>
2024-06-27 00:07:22 +0530

    half done

---
+++ log.js
@@ -0,0 +1,10 @@
+// script.js
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Login form submission logic
+});
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Signup form submission logic
+});
